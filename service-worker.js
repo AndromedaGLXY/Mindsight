09414f3f9d4c26715b68431bd95d5cd884b7caf7
AndromedaGLXY <matthew-elliott@hotmail.co.uk>
2025-09-28 09:44:17 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,21 @@
-
-const CACHE_NAME = 'mindsight-cache-v1';
-const FILES_TO_CACHE = ['.','./index.html','./style.css','./script.js','./manifest.json','./icon-192.png','./icon-512.png'];
-
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('mindsight-cache').then((cache) => {
+      return cache.addAll([
+        '/Mindsight/',
+        '/Mindsight/index.html',
+        '/Mindsight/style.css',
+        '/Mindsight/script.js',
+        '/Mindsight/manifest.json',
+        '/Mindsight/icon-192.png',
+        '/Mindsight/icon-512.png'
+      ]);
+    })
   );
-  self.skipWaiting();
 });
-self.addEventListener('activate', (evt) => { evt.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', (evt) => {
-  evt.respondWith(caches.match(evt.request).then((resp) => resp || fetch(evt.request).catch(()=>caches.match('./index.html'))));
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
